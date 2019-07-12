f75ced1be86bcea382c9dbfcb5d66ed862471f42
Justy Wong <justy.wong@bloombase.com>
2019-07-11 23:57:15 -0700

    minor update

--- src/main/java/jw/pkcs11/SunPKCS11Sample.java
+++ src/main/java/jw/pkcs11/SunPKCS11Sample.java
@@ -1,8 +1,14 @@
-/**
- * How to initialize SunPKCS11 provider on Java 9 or later
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package jw.pkcs11;
 
 import java.security.KeyStore;
@@ -10,6 +16,13 @@ import java.security.Provider;
 import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.util.Enumeration;
+
+/**
+ * How to initialize SunPKCS11 provider on Java 9 or later
+ *
+ * @author justy.wong
+ *
+ */
 
 public class SunPKCS11Sample {
 
